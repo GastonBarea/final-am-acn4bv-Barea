b1007b0d56ce437f00dd329d3fe53a2edd858c6f
Gaston <barea.gaston@gmail.com>
2024-04-29 21:43:21 -0300

    primera prueba realizada con éxito.

--- app/src/main/java/com/example/peluquera/MainActivity.java
+++ app/src/main/java/com/example/peluquera/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.peluquera;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,8 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void aReserva (View view){
+        Log.i("boton_NuevaReserva", "Ganaste");
     }
 }
