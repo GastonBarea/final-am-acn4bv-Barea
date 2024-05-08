c1dca397537e3425ece87db5b62ee878f585a8e6
Gaston <barea.gaston@gmail.com>
2024-05-08 08:20:24 -0300

    feat: Arreglando la barra toolbar por bottomNavigationView

--- app/src/main/java/com/example/peluquera/MainActivity.java
+++ app/src/main/java/com/example/peluquera/MainActivity.java
@@ -7,17 +7,18 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
-
+import androidx.annotation.NonNull;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,12 +31,39 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
-        androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+// Inicializar el BottomNavigationView
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        // Configurar un escuchador de eventos para manejar las acciones del usuario
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        // Acción cuando se selecciona la opción "Home"
+                        Toast.makeText(MainActivity.this, "Home seleccionado", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.book:
+                        // Acción cuando se selecciona la opción "Book"
+                        Toast.makeText(MainActivity.this, "Book seleccionado", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.user:
+                        // Acción cuando se selecciona la opción "User"
+                        Toast.makeText(MainActivity.this, "User seleccionado", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.history:
+                        // Acción cuando se selecciona la opción "History"
+                        Toast.makeText(MainActivity.this, "History seleccionado", Toast.LENGTH_SHORT).show();
+                        return true;
+                }
+                return false;
+            }
+        });
+
+
 
     }
 
-    public boolean onCreateOptionsMenu(Menu menu){
+    /*public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.bottom_menu, menu);
         return true;
     }
@@ -62,7 +90,7 @@ public class MainActivity extends AppCompatActivity {
             return super.onOptionsItemSelected(item);
         }
 
-    }
+    }*/
 
 
     public void aReserva (View view){
