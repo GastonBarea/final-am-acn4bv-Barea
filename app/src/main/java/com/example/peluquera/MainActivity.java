5757226ddd194672bffc47dfc495803bc98b71d3
Gaston <barea.gaston@gmail.com>
2024-05-08 11:42:37 -0300

    feat: boton a reserva

--- app/src/main/java/com/example/peluquera/MainActivity.java
+++ app/src/main/java/com/example/peluquera/MainActivity.java
@@ -1,22 +1,13 @@
 package com.example.peluquera;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
-
-    private BottomNavigationView bottomNavigationView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -29,36 +20,6 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        /*bottomNavigationView = findViewById(R.id.bottomNavigationView);
-        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.home: // Aquí es donde se usa R.id.home como una constante
-                        Toast.makeText(MainActivity.this, "Home seleccionado", Toast.LENGTH_SHORT).show();
-                        return true;
-                    case R.id.book:
-                        Toast.makeText(MainActivity.this, "Book seleccionado", Toast.LENGTH_SHORT).show();
-                        return true;
-                    case R.id.user:
-                        Toast.makeText(MainActivity.this, "User seleccionado", Toast.LENGTH_SHORT).show();
-                        return true;
-                    case R.id.history:
-                        Toast.makeText(MainActivity.this, "History seleccionado", Toast.LENGTH_SHORT).show();
-                        return true;
-                }
-                return false;
-            }
-        });*/
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.bottom_menu, menu);
-        return true;
-    }
-
-    public void aReserva(View view){
-        Log.i("boton_NuevaReserva", "Ganaste");
-    }
 }
--- app/src/main/java/com/example/peluquera/MainActivityHome.java
+++ app/src/main/java/com/example/peluquera/MainActivityHome.java
@@ -1,6 +1,10 @@
 package com.example.peluquera;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,67 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.annotation.NonNull;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
+
 public class MainActivityHome extends AppCompatActivity {
+
+    Button btn_nuevaReserva;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main_home);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.menu), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+//Creando botón para ir a buscar reservas
+        btn_nuevaReserva = findViewById(R.id.boton_NuevaReserva);
+        btn_nuevaReserva.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivityHome.this,MainActivityUser.class);//a donde va
+                startActivities(new Intent[]{intent});
+            }
+        });
+
+//Intentando crear la barra de navegación
+        /*bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        Toast.makeText(MainActivityHome.this, "Home seleccionado", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.book:
+                        Toast.makeText(MainActivityHome.this, "Book seleccionado", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.user:
+                        Toast.makeText(MainActivityHome.this, "User seleccionado", Toast.LENGTH_SHORT).show();
+                        return true;
+                    case R.id.history:
+                        Toast.makeText(MainActivityHome.this, "History seleccionado", Toast.LENGTH_SHORT).show();
+                        return true;
+                }
+                return false;
+            }
+        });*/
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.bottom_menu, menu);
+        return true;
+    }
+
+
+
 }
